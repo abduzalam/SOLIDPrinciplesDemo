b5ca93febb1e32d479dd232af2de957d70ff6dd1
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-27 14:40:38 +0100

    minor edits

--- OCPInitial/Program.cs
+++ OCPInitial/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("OCPInitial!");
+// The Open Closed Principle (OCP) is the SOLID principle which states that the software entities (classes or methods)
+// should be open for extension but closed for modification.
