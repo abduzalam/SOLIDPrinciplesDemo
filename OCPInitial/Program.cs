f82a3fc91c0205744503af384673951a39f65966
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-27 16:55:25 +0100

    ocp initial code changes

--- OCPInitial/Program.cs
+++ OCPInitial/Program.cs
@@ -1,4 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OCPInitial;
+
 Console.WriteLine("OCPInitial!");
 // The Open Closed Principle (OCP) is the SOLID principle which states that the software entities (classes or methods)
 // should be open for extension but closed for modification.
+
+// We are using a Salary Calculator example here
+
+// In the first time, we have only CalculateTotalSalaries() of all employee level in SalaryCalculator class
+
+var devReports = new List<DeveloperReport>()
+{ 
+    new DeveloperReport{ Id = 1, Name = "Dev1", Level = "Senior developer", HourlyRate = 30.5, WorkingHours = 160 },
+    new DeveloperReport {Id = 2, Name = "Dev2", Level = "Junior developer", HourlyRate  = 20, WorkingHours = 150 },
+    new DeveloperReport {Id = 3, Name = "Dev3", Level = "Senior developer", HourlyRate  = 30.5, WorkingHours = 180 }
+};
+
+var calculator = new SalaryCalculator(devReports);
+Console.WriteLine($"Sum of all developer salaries is {calculator.CalculateTotalSalaries()} dollars");
+
+// Assume the above code is in production.
+
+// but now our boss comes to our office and says that we need a different calculation for the senior and junior developers.
+// The senior developers should have a bonus of 20% on a salary.
+
+// Of course, to satisfy this requirement, we are going to modify our CalculateTotalSalaries method 
---
+++ OCPInitial/SalaryCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCPInitial
+{
+    public class SalaryCalculator
+    {
+        private readonly IEnumerable<DeveloperReport> _developerReports;
+        public SalaryCalculator(List<DeveloperReport> developerReports)
+        {
+            _developerReports = developerReports;
+        }
+
+        /// <summary>
+        /// The commented CalculateTotalSalaries is the initial version
+        /// </summary>
+        /// <returns></returns>
+        //public double CalculateTotalSalaries()
+        //{
+        //    double totalSalaries = 0D;
+
+        //    foreach (var devReport in _developerReports)
+        //    {
+        //        totalSalaries += devReport.HourlyRate * devReport.WorkingHours;
+        //    }
+        //    return totalSalaries;
+        //}
+
+
+
+        // Even though this solution is going to give us the correct result, this is not an optimal solution.
+        // Mainly, because we had to modify our existing class behavior which worked perfectly.
+        // Another thing is that if our boss comes again and ask us to modify calculation for the junior dev’s as well,
+        // we would have to change our class again. This is totally against of what OCP stands for.
+        public double CalculateTotalSalaries()
+        {
+            double totalSalaries = 0D;
+
+            foreach (var devReport in _developerReports)
+            {
+                if(devReport.Level == "Senior developer")
+                    totalSalaries += devReport.HourlyRate * devReport.WorkingHours * 1.2;
+                else
+                    totalSalaries += devReport.HourlyRate * devReport.WorkingHours;
+            }
+            return totalSalaries;
+        }
+    }
+}
